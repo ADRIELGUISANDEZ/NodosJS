73237811ec8afbe4d84556584d4b82b46306d571
adriel <adrielguisandez09@gmail.com>
2023-12-11 19:52:00 -0300

    entrega primera

--- Script/mains.js
+++ Script/mains.js
@@ -20,5 +20,24 @@ if (importe <= 150000){
 for (importe > 150000; importe < 1000000; importe = importe + 40000)
 console.log("Ciclo de Cuotas " + importe);
 
+
+
 let localidad = prompt("ingrese localidad")
 alert ("Buenísimo!! tenemos sucursal en " + localidad)
+
+function sumaPrestamo1 (prestamo1, meses){;
+console.log(prestamo1/meses);
+}
+
+function sumaPrestamo2 (prestamo2, meses){;
+console.log(prestamo2/meses);
+}
+
+function sumaPrestamo3 (prestamo3, meses){;
+    console.log(prestamo3/meses);
+    }
+
+
+sumaPrestamo1(400000, 12);
+sumaPrestamo2(600000, 12);
+sumaPrestamo3(1000000, 12);
