70491509d45e3c3e69fd6834a075f3296477a375
adriel <adrielguisandez09@gmail.com>
2023-12-11 19:11:06 -0300

    primer entrega

---
+++ Script/mains.js
@@ -0,0 +1,24 @@
+const nombreUsuario = prompt("ingrese nombre y apellido");
+alert ("Hola " + nombreUsuario + "!! " + "Bienvenido");
+
+let nacimiento = Number(prompt("ingrese su año de nacimiento"))
+alert ("Usted tiene " + (2023 - nacimiento) + " " + "años");
+
+let importe = Number(prompt("ingrese sueldo de bolsillo"))
+
+if (importe <= 150000){
+    alert ("no puede solicitar préstamo");
+} else if (importe > 150000 && importe <= 250000){
+    alert ("usted tiene pre aprobado 200 mil pesos");
+} else if (importe > 250000 && importe <= 350000){
+    alert ("usted tiene pre aprobado 300 mil pesos");
+} else {
+    alert ("usted tiene pre aprobado 500 mil pesos");
+}
+
+
+for (importe > 150000; importe < 1000000; importe = importe + 40000)
+console.log("Ciclo de Cuotas " + importe);
+
+let localidad = prompt("ingrese localidad")
+alert ("Buenísimo!! tenemos sucursal en " + localidad)
